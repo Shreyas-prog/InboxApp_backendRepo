fff6b6ddf97b2e06513b054df39184485e535650
Shreyas-prog <shreyasacharya2000@gmail.com>
2025-07-25 07:48:48 +0530

    removed unwanted comments inn the code.

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -10,7 +10,7 @@ const router = express.Router();
 
 router.get('/get', getAllTasks);
 router.post('/create', createTask);
-router.post('/update', updateTask);    // no :id in path now
-router.post('/delete', deleteTask);    // no :id in path
+router.post('/update', updateTask);
+router.post('/delete', deleteTask);
 
 export default router;
